074765a1bb15a01ecff20d8d1e7d13142f736db3
Peter Allen <peter_james_allen@hotmail.com>
2021-04-06 16:08:03 +0100

    added start and end balance to transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,9 +1,11 @@
 class Transaction
 
-  attr_reader :date, :amount
+  attr_reader :date, :amount, :start_balance, :end_balance
 
-  def initialize(amount)
+  def initialize(amount, start_balance)
     @amount = amount
+    @start_balance = start_balance
+    @end_balance = start_balance + amount
     @date = Time.now.strftime("%m/%d/%Y")
   end
 
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,10 +2,27 @@ require 'transaction'
 
 describe Transaction do
 
-  let(:subject) { Transaction.new(10) }
+  let(:subject) { Transaction.new(10, 0) }
 
   it 'should have a date and amount' do
     expect(subject.date).to eq Time.now.strftime("%m/%d/%Y")
     expect(subject.amount).to eq 10
+  end
+
+  context 'making a deposit' do
+    let(:subject) { Transaction.new(10, 0) }
+
+    it 'should have a start and end balance' do
+      expect(subject.start_balance).to eq 0
+      expect(subject.end_balance).to eq 10
+    end
+  end
+
+  context 'making a withdrawal' do
+  let(:subject) { Transaction.new(-10, 0) }
+    it 'should have a start and end balance' do
+      expect(subject.start_balance).to eq 0
+      expect(subject.end_balance).to eq -10
+    end
   end
 end
