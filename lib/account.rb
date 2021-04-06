370e2a5215f0026a5f91c1cfda27099ff679c79f
Peter Allen <peter_james_allen@hotmail.com>
2021-04-06 15:25:45 +0100

    added input validation

--- lib/account.rb
+++ lib/account.rb
@@ -5,14 +5,24 @@ class Account
   attr_reader :balance
 
   def initialize
-    @balance = 0
+    @balance = 0.00
   end
 
   def deposit(amount)
+    raise 'Error: You must input a number with maximum of 2 decimal places' unless validate_input?(amount)
     @balance += amount
   end
 
   def withdraw(amount)
+    raise 'Error: You must input a number with maximum of 2 decimal places' unless validate_input?(amount)
     @balance -= amount
   end
+
+  private
+  def validate_input?(amount)
+    check1 = amount.instance_of? Integer
+    check2 = (amount.instance_of? Float) && (amount.to_s.split('.').last.length <= 2 )
+    check1 || check2
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -16,4 +16,17 @@ describe Account do
     subject.withdraw(10)
     expect(subject.balance).to eq(-10)
   end
+
+  # Validate inputs
+  context 'should only take a integer or 2dp float as input' do
+    it 'should raise an error if a string is deposited or withdrawn' do
+      expect{ subject.deposit('abc') }.to raise_error'Error: You must input a number with maximum of 2 decimal places'
+      expect{ subject.withdraw('abc') }.to raise_error 'Error: You must input a number with maximum of 2 decimal places'
+    end
+
+    it 'should raise an error if a float with more than 2dp is deposited or withdrawn' do
+      expect{ subject.deposit(1.001) }.to raise_error'Error: You must input a number with maximum of 2 decimal places'
+      expect{ subject.withdraw(1.001) }.to raise_error 'Error: You must input a number with maximum of 2 decimal places'
+    end
+  end
 end
