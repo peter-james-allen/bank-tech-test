be0e7b6b9ab757a85d57f04089bb602287af08f9
Peter Allen <peter_james_allen@hotmail.com>
2021-04-06 14:50:22 +0100

    added deposit method

---
+++ spec/account_spec.rb
@@ -0,0 +1,14 @@
+require 'account'
+
+describe Account do
+
+  it 'should have a balance of zero by default' do
+    expect(subject.balance).to eq 0
+  end
+
+  it 'should be able to make a deposit' do
+    subject.deposit(10)
+    expect(subject.balance).to eq 10
+  end
+
+end
